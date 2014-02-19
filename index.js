a9d5dc919c4689e3ce08704a8001093658dcaa51
poornima <pvenkatakrishnan@paypal.com>
2014-02-18 18:53:56 -0800

    Removing all the stuff about context management that does not belong in kakra

--- index.js
+++ index.js
@@ -1,40 +1,6 @@
 'use strict';
-var spclizer = require('./lib/specializer'),
-    dust = require('dustjs-linkedin');
+var spclizer = require('./lib/specializer');
 
 exports.create = function (config) {
-    var specializer = spclizer.create(config);
-    return {
-        specializer: specializer,
-        renderer:  getSpclWrapper(config, specializer.resolveAll)
-    };
+    return spclizer.create(config);
 };
-
-function getSpclWrapper (config, mapper) {
-    return function (engine) {
-        setupDustOnLoad();
-        return function(file, options, callback) {
-            //generate the specialization map
-            options._specialization =  mapper(options);
-            engine.apply(null, arguments);
-        };
-    }
-}
-
-function setupDustOnLoad() {
-    var originalOnLoad = dust.onLoad,
-        specialization,
-        mappedName;
-    dust.onLoad = function onLoad (name, context, cb) {
-        specialization = (typeof context.get === 'function' && context.get('_specialization')) || context._specialization;
-        mappedName = (specialization && specialization[name] || name);
-        originalOnLoad(mappedName, context, function(err, data) {
-            if (!err && mappedName !== name && typeof data === 'string') {
-                //this is a workaround, since adaro is not aware of the mapped name up the chain
-                //we find the dust.register line and replace the mappedName of template with original name
-                data = data.replace(mappedName, name);
-            }
-            cb(err, data);
-        });
-    }
-}
